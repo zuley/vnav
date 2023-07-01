06462b6d21cc5b8f44bf04e47be292e21a8c094e
zuley <914230482@qq.com>
2023-07-01 18:36:58 +0800

    feat：修改拉取数据的逻辑

---
+++ pages/term/[slug].vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { params: { slug } } = useRoute()
+</script>
+
+<template>
+  <search />
+  <term-box :slug="slug as string" />
+  <post-box :term="slug as string" />
+</template>
+
+<style scoped>
+
+</style>
