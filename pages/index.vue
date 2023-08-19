4a1d65c408f54de9a017785d0c6fdac37265b14f
zuley <914230482@qq.com>
2023-08-19 11:43:41 +0800

    feat: 🎸 添加首页 seo

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,14 @@
 <script setup lang="ts">
 
+// 首页SEO
+const opt = useOption()
+useHead({
+  title: `${opt.get('site-name')}-${opt.get('site-subtitle')}`,
+  meta: [
+    { name: 'description',  content: opt.get('site-description') },
+    { name: 'keywords', content: opt.get('site-keys') }
+  ]
+})
 </script>
 
 <template>
