efd793a180425916cc21b6f97130630639783f2c
zuley <914230482@qq.com>
2023-08-26 17:01:20 +0800

    feat: 🎸 添加备案信息

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-
+const opt = useOption()
 </script>
 
 <template>
   <div class="text-center py-3">
-    <h4 class="text-gray-400">版权所有@VNAV</h4>
+    <h4 class="text-gray-400">
+      版权所有@VNAV -
+      <a href="https://beian.miit.gov.cn" target="_blank" rel="nofollow">{{ opt.get('site-beian') }}</a>
+    </h4>
   </div>
 </template>
 
